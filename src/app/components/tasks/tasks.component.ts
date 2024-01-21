2759843697c4a694155b24a3804904555098d1e9
Navindu Dahanayake <NavinduD@99x.io>
2024-01-21 21:17:57 +0530

    add addTask form toggle functionality

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,44 +1,47 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Task } from '../../Task';
 import { TaskService } from '../../services/task.service';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   tasks: Task[] = [];
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
-   this.taskService.getTasks().subscribe((tasks)=> this.tasks = tasks);
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
   deleteTask(task: Task): void {
     this.taskService
       .deleteTask(task)
       .subscribe(
-        (task)=> this.tasks = this.tasks.filter((t) => t.id !== task.id)
+        (task) => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
       );
   }
 
-  toggleReminder(task:Task): void {
+  toggleReminder(task: Task): void {
     task.reminder = !task.reminder;
-    this.taskService
-      .updateTask(task)
-      .subscribe();
+    this.taskService.updateTask(task).subscribe();
   }
 
   addTask(task: Task): void {
-    const id = this.tasks.length + 1;
-
-    const newTask = { id, ...task };
 
     this.taskService
-      .saveTask(newTask)
+      .saveTask(task)
       .subscribe((task) => this.tasks.push(task));
   }
 }
